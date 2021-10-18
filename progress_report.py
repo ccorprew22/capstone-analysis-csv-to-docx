5381e4f516150fadf885bf870e24b7b55051f2e9
Chris Corprew <ccorprew22@hotmail.com>
2021-10-18 16:32:41 -0400

    Made update to sort projects by exec team member and added more style changes

--- progress_report.py
+++ progress_report.py
@@ -13,10 +13,11 @@ import pandas as pd
 import numpy as np
 from docx import Document
 from docx.shared import Pt
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 document = Document()
 
-reports = pd.read_csv("progress_report_form_3.csv") #Change to valid file name
+reports = pd.read_csv("ProgressreportformCapstoneProg(3).csv") #Change to valid file name
 report_dict = {}
 num = int(input("Enter progress report num: "))
 reports = reports.loc[reports['Progress Report #'] == num]
@@ -25,39 +26,47 @@ print(reports["PM Name"])
 
 """ Dictionary format
 {
-   Row # : num,
-   Row Data: {
-       Project Type: Project Type,
-       Project Title : Project Title,
-       PM Name : Name,
-       Team Members : [Team Member Name: Team member name,
-            data: {
-               Overall: Score,
-               Professionalism: Score,
-               Technical: Score,
-               Communication: Score
-               Promptness: Score,
-               Ability to Get Along: Score,
-               Ability to Learn: Score
-           },
-           ...
-           ]
-      Comments : comment
+   Row #num': {
+       Row # : num
+       Row Data: {
+           Project Type: Project Type,
+           Project Title : Project Title,
+           Executive: Name,
+           PM Name : Name,
+           Team Members : [
+                {
+                    Team Member Name: Team member name,
+                    data: {
+                       Overall: Score,
+                       Professionalism: Score,
+                       Technical: Score,
+                       Communication: Score
+                       Promptness: Score,
+                       Ability to Get Along: Score,
+                       Ability to Learn: Score
+                    }
+               },
+               ...
+          ]
+          Comments : comment
+       }
    }
 }
 """
 reports_cols = list(reports.columns)
 
 project_type = [reports_cols[3]]
-pm_name = [reports_cols[5]]
-tm_1_evals = reports_cols[9:19]
-tm_2_evals = reports_cols[19:28]
-tm_3_evals = reports_cols[28:37]
-tm_4_evals = reports_cols[37:46]
-tm_5_evals = reports_cols[46:55]
+executive = [reports_cols[4]]
+pm_name = [reports_cols[6]]
+project_name = [reports_cols[10]]
+tm_1_evals = reports_cols[11:20]
+tm_2_evals = reports_cols[20:29]
+tm_3_evals = reports_cols[29:38]
+tm_4_evals = reports_cols[38:47]
+tm_5_evals = reports_cols[47:56]
 comments_col = ["Any obstacles or problems facing your project?"]
 team_member_evals = tm_1_evals+tm_2_evals+tm_3_evals+tm_4_evals+tm_5_evals
-working_cols = project_type + pm_name + team_member_evals+comments_col #takes desired columns
+working_cols = project_type + executive + pm_name + project_name + team_member_evals+comments_col #takes desired columns
 
 num_rows = reports.index.tolist()
 
@@ -89,6 +98,7 @@ for i in num_rows:
     row_dict = None
     project_type = row["What type of project do you have?"]
     project_title = row["Project Title"]
+    executive = row["Executive Team Mentor"]
     tm_list = []
     for j in range(1,6):
         tm_dict = {}
@@ -143,6 +153,7 @@ for i in num_rows:
             "Project Type" : project_type,
             "Project Title" : project_title,
             "PM Name" : pm,
+            "Executive": executive,
             "Team Members" : tm_list,
             "Comments": comment
         }
@@ -150,67 +161,92 @@ for i in num_rows:
     report_dict["Row #{}".format(i)] = row_dict
 #print(report_dict)
 
-""" Puts information into docx file in desired format"""
-for row in report_dict:
-    row_num = report_dict[row]
-    r_data = row_num["Row Data"]
-    #print(r_data)
+#exec_list = list(tm_reports["Exectutive Team Mentor"].unique())
 
-    #Makes project title bold
-    p_title = "{} ({})".format(r_data["Project Title"], r_data["Project Type"])
-    p_pm_name = "PM: {}".format(r_data["PM Name"])
-    p_tm_title = "Team Members:"
+def docx_print(d):
+    exec_d = {}
+    for row in d:
+        row_num = d[row]
+        r_data = row_num["Row Data"]
 
-    p = document.add_paragraph()
-    p.paragraph_format.space_before = Pt(3)
-    p.paragraph_format.space_after = Pt(3)
-    p_bold = p.add_run(p_title)
-    p_bold.bold = True
+        proj_title = "{} ({})".format(r_data["Project Title"], r_data["Project Type"])
+        pm_n = "PM: {}".format(r_data["PM Name"])
+        team = []
 
-    p_pm_name = document.add_paragraph(p_pm_name, style='List Bullet 2')
-    p_pm_name.paragraph_format.space_before = Pt(3)
-    p_pm_name.paragraph_format.space_after = Pt(3)
+        for n in r_data["Team Members"]:
 
-    p_tm_title = document.add_paragraph(p_tm_title, style='List Bullet 2')
-    p_tm_title.paragraph_format.space_before = Pt(3)
-    p_tm_title.paragraph_format.space_after = Pt(3)
-
-    for n in r_data["Team Members"]:
-        tm_name = "{}".format(n["Team Member Name"])
-        score = 0
-        #print(type(n["data"]["Overall"]))
-        try:
-            # score += int(n["data"]["Overall"])
-            # score += int(n["data"]["Professionalism"])
-            # score += int(n["data"]["Technical"])
-            # score += int(n["data"]["Communication"])
-            # score += int(n["data"]["Promptness"])
-            # score += int(n["data"]["Ability to Get Along"])
-            # score += int(n["data"]["Ability to Learn"])
-            data_list = list(n["data"].values())
-            data_list_int = [int(s) for s in data_list]
-            score += sum(data_list_int)
-        except ValueError:
-            score = np.nan
-        tm_name = tm_name.rsplit('\t', 1)[0]
-        #print(tm_name + ' ' + str(score))
-        tm_name += " ({}/70)".format(str(score))
-        tm_name = document.add_paragraph(tm_name, style='List Bullet 3')
-        tm_name.paragraph_format.space_before = Pt(3)
-        tm_name.paragraph_format.space_after = Pt(3)
-
-    comment_title = document.add_paragraph("Comments/Issues", style='List Bullet 2')
-    comment_title.paragraph_format.space_before = Pt(3)
-    comment_title.paragraph_format.space_after = Pt(3)
-
-    comment_text = r_data["Comments"]
-    if r_data["Comments"] is np.nan:
-        comment_text = "N/A"
-    else:
+            tm_name = "{}".format(n["Team Member Name"])
+            score = 0
+            try:
+                data_list = list(n["data"].values())
+                data_list_int = [int(s) for s in data_list]
+                score += sum(data_list_int)
+            except ValueError:
+                score = np.nan
+            team.append({"name": tm_name, "score": score})
         comment_text = r_data["Comments"]
-    comments = document.add_paragraph(comment_text, style='List Bullet 3')
-    comments.paragraph_format.space_before = Pt(3)
-    comments.paragraph_format.space_after = Pt(3)
-    space = document.add_paragraph("\n")
+        if r_data["Executive"] in exec_d:
+            exec_d[r_data["Executive"]].append({"proj_title": proj_title, "pm_n": pm_n, "team" : team, "comment": comment_text})
+        else:
+            exec_d[r_data["Executive"]] = [{"proj_title": proj_title, "pm_n": pm_n, "team" : team, "comment": comment_text}]
+    return exec_d
+
+""" Puts information into docx file in desired format"""
+docx_dict = docx_print(report_dict)
+exec_list = list(docx_dict.keys())
+print(docx_dict.keys())
+print(exec_list)
+for name in exec_list:
+    exec_title = document.add_paragraph()
+    exec_title.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    exec_runner = exec_title.add_run(name)
+    exec_runner.bold = True
+    exec_runner.underline = True
+    font = exec_runner.font
+    font.size = Pt(15)
+
+    for proj in docx_dict[name]:
+        p_title = proj["proj_title"]
+        p_pm_name = proj["pm_n"]
+        p_tm_title = "Team Members:"
+
+        p = document.add_paragraph()
+        p.paragraph_format.space_before = Pt(3)
+        p.paragraph_format.space_after = Pt(3)
+        p_bold = p.add_run(p_title)
+        p_bold.bold = True
+
+        p_pm_name = document.add_paragraph(p_pm_name, style='List Bullet 2')
+        p_pm_name.paragraph_format.space_before = Pt(3)
+        p_pm_name.paragraph_format.space_after = Pt(3)
+
+        p_tm_title = document.add_paragraph(p_tm_title, style='List Bullet 2')
+        p_tm_title.paragraph_format.space_before = Pt(3)
+        p_tm_title.paragraph_format.space_after = Pt(3)
+
+        for n in proj["team"]:
+            tm_name = "{}".format(n["name"])
+            score = n["score"]
+
+            tm_name = tm_name.rsplit('\t', 1)[0]
+            #print(tm_name + ' ' + str(score))
+            tm_name += " ({}/70)".format(str(score))
+            tm_name = document.add_paragraph(tm_name, style='List Bullet 3')
+            tm_name.paragraph_format.space_before = Pt(3)
+            tm_name.paragraph_format.space_after = Pt(3)
+
+        comment_title = document.add_paragraph("Comments/Issues", style='List Bullet 2')
+        comment_title.paragraph_format.space_before = Pt(3)
+        comment_title.paragraph_format.space_after = Pt(3)
+
+        comment_text = proj["comment"]
+        if proj["comment"] is np.nan:
+            comment_text = "N/A"
+        else:
+            comment_text = proj["comment"]
+        comments = document.add_paragraph(comment_text, style='List Bullet 3')
+        comments.paragraph_format.space_before = Pt(3)
+        comments.paragraph_format.space_after = Pt(3)
+        #space = document.add_paragraph("\n")
 
 document.save("progress_report_output/progress_report_#{}.docx".format(str(num)))
